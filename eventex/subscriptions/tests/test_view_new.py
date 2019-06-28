93c5070b28b0f9c5dc4082c21bad488e50a7f492
MarioGN <mario.geroldineto@gmail.com>
2019-06-28 11:52:25 -0300

    Email ou telefone opcionais

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -77,3 +77,11 @@ class SubscriptionNewPostInvalid(TestCase):
 
     def test_dont_save_subscriptions(self):
         self.assertFalse(Subscription.objects.exists())
+
+
+class TestRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='Mario Neto', cpf='12345678910')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+
+        self.assertContains(response, '<ul class="errorlist nonfield">')
